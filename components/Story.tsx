e98e3fe79ac77cf1a9cfd3caf6d0b81e3921b655
austincalvelage <austin.calvelage@icloud.com>
2022-08-19 00:12:13 -0400

    Added message into its own component, Added favicon, removed Logo.tsx  (non-used component)

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,36 +1,10 @@
 import type { NextPage} from "next";
-import { motion } from "framer-motion";
-import classNames from 'classnames';
 
 import { IoIosCamera, IoIosAppstore } from 'react-icons/io'
 import { FaArrowCircleUp } from 'react-icons/fa'
 
-import Logo from "./Logo"
+import Message from "./Message";
 
-interface Props {
-    time?: string,
-    text: string,
-    response: boolean,
-    animate?: object,
-    transition?: object,
-    initial?: object,
-  }
-
-  const Message: NextPage<Props> = ({time, text, response, animate, transition, initial }) => {
-    return (
-        <motion.div
-            initial={initial}
-            className="flex flex-col space-y-2"
-            animate={animate}
-            transition={transition}
-        >
-            <span className="text-[#8A8A8D] self-center text-xs">{time}</span>
-            <p 
-                className={classNames("text-xs p-2 rounded-lg w-fit max-w-[65%] shadow-md", response ? "bg-[#6ACE6B] text-white self-end" : "bg-[#E9E9EB] text-black self-start")}>
-                {text}
-            </p>
-        </motion.div>)
-  }
 
 const Story: NextPage = () => {
     return (
